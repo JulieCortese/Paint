369c74681d8de00fcb5bc419a661df3de74ef47c
julie <jcortese@ufl.edu>
2022-12-30 18:24:33 -0500

    .

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
 # this is paint
 import pygame
 import sys
-WIDTH = 600
-HEIGHT = 600
-BG_COLOR = (255, 245, 218)
+WIDTH = 1200
+HEIGHT = 800
+BG_COLOR = (255, 255, 255)
 LINE_COLOR = (0, 0, 0)
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill((255, 255, 255))
+screen.fill(BG_COLOR)
 pygame.display.set_caption('J-Paint')
 pygame.display.update()
 pygame.time.wait(600)
