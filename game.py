f0c06f0c97ef6bfc0b57ccdcf796cfa5b09da4ed
julie <jcortese@ufl.edu>
2022-12-30 19:05:50 -0500

    .

--- game.py
+++ game.py
@@ -4,11 +4,19 @@ import sys
 WIDTH = 1200
 HEIGHT = 800
 BG_COLOR = (255, 255, 255)
-LINE_COLOR = (0, 0, 0)
+COLOR = (0, 0, 0)
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(BG_COLOR)
 pygame.display.set_caption('J-Paint')
 pygame.display.update()
-pygame.time.wait(600)
+restart = False
+while not restart:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('yes')
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            pygame.quit()
+            sys.exit()
