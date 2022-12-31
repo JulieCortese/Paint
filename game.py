a00b0e69ba9f8b8447e798982d06de8cb9a98d13
julie <jcortese@ufl.edu>
2022-12-31 00:56:42 -0500

    gotta keep working on the clickable colors

--- game.py
+++ game.py
@@ -25,55 +25,29 @@ def res_button(screen):
     reset_rect = reset_surf.get_rect(center=(1100, 750))
     screen.blit(reset_surf, reset_rect)
 
-    change_text = button_font.render('change color', True, (0, 0, 0))
-    change_surf = pygame.Surface((change_text.get_size()[0] + 20, change_text.get_size()[1] + 20))
-    change_surf.fill((235, 148, 9))
-    change_surf.blit(change_text, (10, 10))
-    change_rect = change_surf.get_rect(center=(900, 750))
-    screen.blit(change_surf, change_rect)
+    erase_text = button_font.render('Erase', True, (0, 0, 0))
+    erase_surf = pygame.Surface((erase_text.get_size()[0] + 20, erase_text.get_size()[1] + 20))
+    erase_surf.fill((235, 148, 9))
+    erase_surf.blit(erase_text, (10, 10))
+    erase_rect = erase_surf.get_rect(center=(900, 750))
+    screen.blit(erase_surf, erase_rect)
 
     pygame.display.update()
-    return reset_rect, change_rect
+    return reset_rect, erase_rect
 
 
-def color_change(screen):
-    screen.fill(BG_COLOR)
-    big_font = pygame.font.Font(None, 70)
-    title_surface = big_font.render('Color Pick', 0, (0, 0, 0))
-    title_rectangle = title_surface.get_rect(
-        center=(WIDTH // 2, HEIGHT // 2 - 300))
-    screen.blit(title_surface, title_rectangle)
+def color_options(screen):
+    pygame.draw.circle(screen, (0, 0, 0), center=(80, 750), radius=30.0)
+    pygame.draw.circle(screen, 'red', center=(80, 750), radius=20.0)
 
-    done_text = button_font.render('Paint', True, (0, 0, 0))
-    done_surf = pygame.Surface((done_text.get_size()[0] + 20, done_text.get_size()[1] + 20))
-    done_surf.fill((235, 148, 9))
-    done_surf.blit(done_text, (10, 10))
-    done_rect = done_surf.get_rect(center=(1100, 750))
-    screen.blit(done_surf, done_rect)
-    pygame.display.update()
 
-    done = False
-    while True and not done:
-        timer.tick(fps)
-        pygame.draw.rect(screen, color, [WIDTH // 2 - 200, HEIGHT // 2 - 200, 400, 400])
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if done_rect.collidepoint(event.pos):
-                    screen.fill(BG_COLOR)
-                    reset_button, color_change_button = res_button(screen)
-                    done = True
-                    break
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-                sys.exit()
 
 
 while run:
     timer.tick(fps)
     mouse_pos = pygame.mouse.get_pos()
-    reset_button, color_change_button = res_button(screen)
+    reset_button, erase_button = res_button(screen)
+    colors = color_options(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -82,9 +56,13 @@ while run:
                 reset_button, color_change_button = res_button(screen)
                 pygame.display.flip()
                 continue  # makes it so you can't click and drag from the reset button
-            if color_change_button.collidepoint(event.pos):
+            elif erase_button.collidepoint(event.pos):
+                color = (255, 255, 255)
                 # make way of changing color (in a different screen maybe)
-                color_change(screen)
+                draw = True
+                continue
+            elif colors[0].collidepoint(event.pos):
+                print('clicked')
                 continue
             draw = True
         elif event.type == pygame.MOUSEBUTTONUP:
